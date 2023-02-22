551725237572799a8aa31930e70d3c1f70d372ea
Shents <59866813+Shents@users.noreply.github.com>
2023-02-22 21:47:35 +1000

    bata-1.0
    можно считать более мение стабилой версией

--- opencl.py
+++ opencl.py
@@ -1,26 +1,20 @@
-import numpy as np 
-import pyopencl as cl 
-import os 
-# Загрузка данных и подготовка к обучению 
-data = np.loadtxt('bnb.csv', delimiter=',') 
-X = data[:, 0:4] # Входные данные 
-y = data[:, 4] # Целевые данные 
-# Настройка OpenCL 
-os.environ['PYOPENCL_CTX'] = '0' # Установка нужного GPU-устройства 
-ctx = cl.create_some_context() # Создание контекста OpenCL 
-queue = cl.CommandQueue(ctx) # Создание очереди OpenCL 
-mf = cl.mem_flags # Флаги памяти OpenCL 
-# Преобразование X, y в OpenCL-буферы 
-X_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=X) 
-y_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=y)  
-# Определение функции OpenCL, которую мы будем использовать 
-prg = cl.Program(ctx, """   __kernel void predict(__global float *X, __global float *y, __global float *pred) {   int gid = get_global_id(0);   pred[gid] = 0;   for (int i=0; i<4; i++) {     pred[gid] += X[gid*4+i] * y[i];   } } """).build() 
-# Выделяем буферы, соответствующие прогнозу 
-pred_buf = cl.Buffer(ctx, mf.WRITE_ONLY, y.nbytes) 
-# Запуск OpenCL-ядра с X, y-буферы  
-prg.predict(queue, X.shape, None, X_buf, y_buf, pred_buf)  
-# Копирование OpenCL-буфера pred_buf в numpy-массив pred 
-pred = np.empty_like(y)  
-cl.enqueue_copy(queue, pred, pred_buf).wait()  
-# Вывод результатов 
-print("Прогноз:", pred)
+import pandas as pd
+import tensorflow as tf
+import numpy as np
+import opencl
+# Загрузка данных для обучения
+data = pd.read_csv('bnb.csv')
+# Преобразование данных в массивы NumPy
+X_train = data.iloc[:, 1:5].values
+y_train = data.iloc[:, 5].values
+# Определение структуры модели
+model = tf.keras.Sequential()
+model.add(tf.keras.layers.Dense(4, activation='relu'))
+model.add(tf.keras.layers.Dense(1))
+# Компиляция модели
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+# Обучение модели
+with opencl.OpenCLContext() as cl_context:
+    model.fit(X_train, y_train, epochs=10, batch_size=32, use_opencl=True, opencl_context=cl_context)
+# Сохранение модели
+model.save('crypto_model.h5')
